2596e0489f8877485acfb433c92afb8603bb2e14
MyAudit <110626461+MyAudit@users.noreply.github.com>
2023-09-11 15:05:22 +0300

    commit - password

--- MyApp.py
+++ MyApp.py
@@ -1,25 +1,8 @@
-# 1) СОЗДАНИЕ VSCODE И РАЗМЕЩЕНИЕ В STREAMLIT
-# Streamlit с инструкцией по деплою, который применен ниже в этой программе
-# https://streamlit.io/
-
-# 2) Видео пример как деплоить из ютуба
-# https://www.youtube.com/watch?v=0kLJ43KATb0
-
-# 3) ОБРАБОТКА В GIT
-# https://streamlit.io/cloud
-
-
-#  Warning: to view this Streamlit app on a browser, run it with the following
-#  command:
-
-#    streamlit run streamlit run c:/Users/User/Documents/IDE/MyApp.py
-# >> [ARGUMENTS]er>
-
 #  You can now view your Streamlit app in your browser.
 
 #  Local URL: http://localhost:8501
 #  Network URL: http://192.168.34.152:8501
-
+  
 # РАЗВОРАЧИВАЕМ СТРИМЛИТ-
 import streamlit as st
 import pandas as pd
@@ -35,25 +18,9 @@ st.write("""
          Аналитическая часть *для инспекторов*
          """)
 
-df = pd.DataFrame({'ГРБС':['056','149','388'],'Вид расходов':[500,600,400],'БА':[10000.0,20000.0,30000.0]})
-st.write(df)
-
-
-# появляется команда которую надо скопировать и запустить здесь же в терминале
-# после чего появляется хост
-
-#  Warning: to view this Streamlit app on a browser, run it with the following
-#  command:
-
-#    streamlit run streamlit run c:/Users/User/Documents/IDE/MyApp.py
-# >> [ARGUMENTS]er>
-
-#  You can now view your Streamlit app in your browser.
-
-#  Local URL: http://localhost:8501
-#  Network URL: http://192.168.34.152:8501
-
-
-
-# ПОПЫТКА ЗАКОММИТИТЬ
-
+password = st.text_input('Для доступа введите пароль:')
+if password != 'inna':
+    st.write('Нет доступа')
+else:
+    df = pd.DataFrame({'ГРБС':['056','149','388'],'Вид расходов':[500,600,400],'БА':[10000.0,20000.0,30000.0]})
+    st.write(df)
