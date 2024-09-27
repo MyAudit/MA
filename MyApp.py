7dbb2302b598c3b8990de2a87f3995e07b6f84aa
MyAudit <mco.winners@gmail.com>
2024-09-27 14:57:47 +0300

    3 checkbox for enter in table

--- MyApp.py
+++ MyApp.py
@@ -1,3 +1,5 @@
+
+
 # C:\Users\User>cd C:\Users\User\Documents\IDE_Github
 # C:\Users\User\Documents\АЦСПРФ>streamlit run MyApp.py
  
@@ -16,21 +18,36 @@ st.set_page_config(
     layout="wide"
 )
 
-st.header('Аналитический центр - MyAudit')
-st.subheader('Комплексная модель автоматизированного поиска рисков нарушений')
-st.write('(переход от выборочных проверок к сплошному автоматизированному контролю федерального бюджета)') 
+st.header('Аналитический портал')
+# st.subheader('Комплексная модель автоматизированного поиска рисков нарушений')
+# st.write('(переход от выборочных проверок к сплошному автоматизированному контролю федерального бюджета)') 
 
 
-st.write('Доступ ограничен')
+# st.write('Доступ ограничен')
 password = st.text_input('Для доступа введите пароль:', type='password')
 if password != 'Inna':
     st.write('НЕТ ДОСТУПА')
 else:
+    gsheet = st.sidebar.checkbox('gsheet прямо')
+    if gsheet:
+        st.subheader('Анализ gsheet прямо')
+        # df = pd.read_excel('https://docs.google.com/spreadsheets/d/1C7UYTDLsPcgmngE45VEH17buU8onkU1q/edit?usp=sharing&ouid=100184939205193698727&rtpof=true&sd=true')
+        df = pd.read_excel('https://docs.google.com/spreadsheets/d/1C7UYTDLsPcgmngE45VEH17buU8onkU1q/export?format=csv&gid=1017825111')
+        st.write(df)
+        
+        
+    gsheet = st.sidebar.checkbox('gsheet через git')
+    if gsheet:
+        st.subheader('Анализ gsheet через git')
+        # df = pd.read_excel('https://docs.google.com/spreadsheets/d/1C7UYTDLsPcgmngE45VEH17buU8onkU1q/edit?usp=sharing&ouid=100184939205193698727&rtpof=true&sd=true')
+        df = pd.read_excel('https://docs.google.com/spreadsheets/d/1C7UYTDLsPcgmngE45VEH17buU8onkU1q/export?format=csv&gid=1017825111')
+        st.write(df)
     
-    df = pd.read_excel('https://github.com/MyAudit/MA/raw/master/!%202023%2010%2001%20faip_sravnenie_k%202023%2010%2025_1.xlsx')
-    
-    st.subheader('Анализ ФАИП на 01.10.2023')
-    st.write(df)
+    git = st.sidebar.checkbox('git файл и папки')
+    if git:
+        st.subheader('Анализ git файл и папки')
+        df = pd.read_excel('https://github.com/MyAudit/MA/raw/master/!%202023%2010%2001%20faip_sravnenie_k%202023%2010%2025_1.xlsx')
+        st.write(df)
     
    
 
